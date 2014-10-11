356a2a15ac44010f47d3f25ee86a2c29af7ebc14
linoox <pd2614g@gmail.com>
2014-10-11 12:14:06 -0400

    modified soln but TLE

--- divide-two-integers/Solution.java
+++ divide-two-integers/Solution.java
@@ -1,27 +1,38 @@
 public class Solution {
 
-	// TLE :(
-
+	// TLE again :/
     public static int divide(int dividend, int divisor) {
-        System.out.println("dividend:"+dividend+" | divisor:"+divisor);
-
-        if (dividend == 0) {
-        	return 0;
-        }	
 
         if (divisor == 0) {
         	throw new IllegalArgumentException("Argument 'divisor' is 0");
         }
 
-        int result=-1;
+        if (dividend == 0) {
+        	return 0;
+        }	
+
+        int result=0;
         int sign = 1;
+        int current=1;
         sign=(dividend/Math.abs(dividend))*(divisor/Math.abs(divisor));
         dividend=Math.abs(dividend);
         divisor=Math.abs(divisor);
 
-        while(dividend >=0) {
-        	dividend = dividend-divisor;
-        	result++;
+        while(divisor <= dividend) {
+        	divisor <<= 1;
+        	current <<= 1;
+        }
+
+        divisor >>= 1;
+        current >>= 1;
+
+        while(current != 0) {
+        	if (dividend >= divisor) {
+        		dividend -= divisor;
+        		result |= current;
+        	}
+        	current >>= 1;
+        	divisor >>= 1;
         }
 
         return result*sign;
@@ -30,8 +41,8 @@ public class Solution {
 
     public static void main(String[] args) {
     
-    	int dividend = 2147483647;
-    	int divisor =  1;
+    	int dividend = -75;
+    	int divisor =  5;
 
     	System.out.println(divide(dividend,divisor));
 
