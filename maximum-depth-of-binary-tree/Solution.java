a5957928e111be4eb239c0627e55055af26fa01c
linoox <pd2614g@gmail.com>
2014-11-11 22:01:55 -0500

    max depth of binary tree

--- maximum-depth-of-binary-tree/Solution.java
+++ maximum-depth-of-binary-tree/Solution.java
@@ -10,11 +10,17 @@
 
 public class Solution {
     public int maxDepth(TreeNode root) {
-        
+     	
+     	if (root == null) {
+     		return 0;
+     	}   
+
+     	return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
     }
 
 
     public static void main(String[] args) {
-    	
-    }
+		
+		// todo construct function
+	}
 }
