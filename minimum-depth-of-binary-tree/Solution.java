4d72b9f783ed1b151a92aed1c57ac53b14350e3f
linoox <pd2614g@gmail.com>
2014-11-13 23:44:58 -0500

    min depth inc

---
+++ minimum-depth-of-binary-tree/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+// TODO
+public class Solution {
+    public int minDepth(TreeNode root) {
+        
+        if(root.left != null || depth <= minDepth) {
+            traverser = root.left;
+        } else {
+            
+        }
+        
+        
+    }
+}
