99394f143fc61cad4885ae9e7d2d2f084bcf310f
linoox <pd2614g@gmail.com>
2015-02-20 23:19:30 -0500

    min depth of binary tree

--- minimum-depth-of-binary-tree/Solution.java
+++ minimum-depth-of-binary-tree/Solution.java
@@ -7,17 +7,43 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
-// TODO
+ 
+ // tweaked hasPathSum code slightly.. there got to be a better solution
 public class Solution {
+    
     public int minDepth(TreeNode root) {
+        int minHeight=0;
+        if (null == root) {
+            return minHeight;
+        }
         
-        if(root.left != null || depth <= minDepth) {
-            traverser = root.left;
-        } else {
+        Queue<TreeNode> nodeList = new LinkedList<TreeNode>();
+        Queue<Integer> incrementalHeight = new LinkedList<Integer>();
+        
+        nodeList.add(root);
+        incrementalHeight.add(1);
+        
+        while(!nodeList.isEmpty()) {
+            TreeNode currentNode = nodeList.poll();
+             int currentHeight = incrementalHeight.poll();
+            
+            if((currentNode.left == null && currentNode.right == null)) {
+               minHeight=currentHeight;
+               return minHeight; 
+            } 
+            
+            if(currentNode.left != null) {
+                nodeList.add(currentNode.left);
+                incrementalHeight.add(currentHeight+1);
+            }
+            
+            if(currentNode.right != null) {
+                nodeList.add(currentNode.right);
+                incrementalHeight.add(currentHeight+1);
+            }
             
         }
         
-        
+        return minHeight;
     }
 }
