279e4f210eef0361412b400b356843ea4af946ba
linoox <pd2614g@gmail.com>
2015-02-26 00:10:46 -0500

    peak detection boundary case failing

--- find-peak-element/Solution.java
+++ find-peak-element/Solution.java
@@ -3,15 +3,25 @@ public class Solution {
     // imp test cases: {1}, {1,2}
     // check prev versions of this code
     public static int findPeakElement(int[] num) {
+        
+        if(num.length == 1) {
+            return 0;
+        } else if (num.length == 2) {
+            if(num[0] > num[1]) return 0;
+
+            return 1;
+        }
+
         int lo = 0;
         int hi = num.length-1;
         int mid = 0;
+
         while (lo <= hi) {
-            mid = lo+(hi-lo)/2;
+            mid = lo+(hi-lo)/2;            
+
             if(num[mid] >= num[mid+1] && num[mid] >= num[mid-1]) return mid;
             
-            if(num[mid] <= num[mid+1]) {
-                // a peak is in the other half
+            if(num[mid] <= num[mid+1]) {                
                 lo = mid+1;
             } else {
                 hi = mid-1;
@@ -21,7 +31,7 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        int[] num = {1, 2};
+        int[] num = {3, 2, 1};
         System.out.println(findPeakElement(num));
     }
 }
