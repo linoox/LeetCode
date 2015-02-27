cffc68c178c1cd229c6f3de04d7cc5e1d562f155
linoox <pd2614g@gmail.com>
2015-02-26 20:14:09 -0500

    find peak inc

--- find-peak-element/Solution.java
+++ find-peak-element/Solution.java
@@ -1,12 +1,12 @@
 public class Solution {
 
-    // imp test cases: {1}, {1,2}
+    // imp test cases: {1}, {1,2}, {3,2,1}
     // check prev versions of this code
     public static int findPeakElement(int[] num) {
-        
-        if(num.length == 1) {
+        int len = num.length;
+        if(len == 1) {
             return 0;
-        } else if (num.length == 2) {
+        } else if (len == 2) {
             if(num[0] > num[1]) return 0;
 
             return 1;
@@ -17,16 +17,27 @@ public class Solution {
         int mid = 0;
 
         while (lo <= hi) {
+            if(hi == lo) {
+                return hi;
+            }
+
             mid = lo+(hi-lo)/2;            
 
-            if(num[mid] >= num[mid+1] && num[mid] >= num[mid-1]) return mid;
+            if(mid == 0) {
+                return mid+1;
+            }
+
             
-            if(num[mid] <= num[mid+1]) {                
+            if((num[mid] >= num[mid+1]) && (num[mid] >= num[mid-1])) 
+                return mid;
+            
+            if(num[mid] < num[mid+1]) {                
                 lo = mid+1;
             } else {
                 hi = mid-1;
             }
         }
+        
         return mid;
     }
 
