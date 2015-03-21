6f31cddef6da96fcb42015b54b2a915548810b99
linoox <pd2614g@gmail.com>
2015-03-20 20:54:24 -0400

    balanced binary tree

--- balanced-binary-tree/Solution.java
+++ balanced-binary-tree/Solution.java
@@ -9,6 +9,25 @@
  */
 public class Solution {
     public boolean isBalanced(TreeNode root) {
+       if (root == null)  {
+           return true;
+       }
+       
+       int heightLeft = height(root.left);
+       int heightRight = height(root.right);
+       
+       // every subtree must be height balanced
+       if (Math.abs(heightLeft - heightRight) <=1 && isBalanced(root.left) && isBalanced(root.right))
+            return true;
         
+        return false;
+    }
+    
+    int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return 1 + Math.max(height(node.left),height(node.right));
+        }
     }
 }
