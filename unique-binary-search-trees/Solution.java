426e554e4b0dff0c4609f280ebd685303944b56b
linoox <pd2614g@gmail.com>
2015-03-26 20:41:25 -0400

    unique BST TLE

---
+++ unique-binary-search-trees/Solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+
+    // todo: TLE - modify
+
+    public int numTrees(int n) {
+        
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        
+        int count = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            int left = numTrees(i-1);
+            int right = numTrees(n-i);
+            count += left*right;
+        }
+        
+        return count;
+    }
+}
