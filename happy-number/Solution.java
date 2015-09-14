d6977197165fef8d0bbe54728e4930251055cf2f
linoox <pd2614g@gmail.com>
2015-09-14 19:37:25 -0400

    happy number

---
+++ happy-number/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+      public static boolean isHappy(int n) {
+   int sum=0;
+   Set<Integer> seenSet = new HashSet<Integer>();
+		while (n > 0) {
+			int x = n%10;
+			sum+=x*x;
+			n=n/10;
+			
+			if (n == 0 && sum != 1) {
+				n=sum;
+				sum=0;
+				seenSet.add(n);
+			} else if (n== 0 && sum == 1 ) {
+				return true;
+			}
+				else if (seenSet.contains(n)) {
+				return false;
+			}
+		}
+		
+		return true;
+   }
+}
